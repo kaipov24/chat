c5ac22254159abb22ca0150b3de0f61e096e42c2
kaipov24 <kaipovich24@gmail.com>
2025-06-14 19:02:34 +0600

    history

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
-import React, { Component } from "react"
+import { Component } from "react"
 import "./App.css"
 import { connect, sendMsg } from "./api"
+import Header from "./components/Header"
+import ChatHistory from "./components/ChatHistory";
+
 
 class App extends Component {
   constructor(props) {
-    super(props)
-    connect()
+    super(props);
+    this.state = {
+      chatHistory: []
+    }
+  }
+
+  componentDidMount() {
+    connect((msg) => {
+      console.log("New Message")
+      this.setState(prevState => ({
+        chatHistory: [...this.state.chatHistory, msg]
+      }))
+      console.log(this.state);
+    });
   }
 
   send() {
@@ -13,13 +28,15 @@ class App extends Component {
     sendMsg("hello")
   }
 
-  render() {
-    return (
-      <div className="App">
-        <button onClick={this.send}>Hit</button>
-      </div>
-    )
-  }
+render() {
+  return (
+    <div className="App">
+      <Header />
+      <ChatHistory chatHistory={this.state.chatHistory} />
+      <button onClick={this.send}>Hit</button>
+    </div>
+  );
+}
 }
 
 export default App
